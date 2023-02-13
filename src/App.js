ed494c547eed9f2a3d440403540114c719070658
Simon Folkerts <simfolkerts@gmail.com>
2023-02-14 11:58:33 +1300

    add title and status props passed from App to ToDo so that ToDo instances can render different data based on information from the parent

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import "./App.css";
 import ToDo from "./components/ToDo.js";
 
 function App() {
+  // a new string, stored in a variable. We can pass this down to the ToDo child component by using props
+  const myString = "hello";
+
   return (
     <div className="App">
       <h1>My To-Do List</h1>
       <section>
         <h2>To-Do List</h2>
         <div className="todos__container">
-          <ToDo />
-          <ToDo />
-          <ToDo />
-          <ToDo />
-          <ToDo />
+          {/* here we have added a pair of props to this instance of ToDo, the first one is called title and passes down a variable's value, the second is called status and is simply passing a string. These will be bundled together into a single object which can be accessed in the component */}
+          <ToDo title={myString} status="completed" />
+
+          {/* here, another instance of ToDo is being passed the same props but with different values. This means that when this instance of ToDo accesses title and status, it will recieve these different values  */}
+          <ToDo title="Go to Gym" status="incomplete" />
         </div>
       </section>
     </div>
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,9 +1,12 @@
 import "./ToDo.css";
-function ToDo() {
+// by adding the 'props' parameter to the component function, it can now access incoming data from the parent
+function ToDo(props) {
   return (
     <div className="todo">
-      <h3 className="todo__title">Placeholder To-Do</h3>
-      <p>Status: Complete</p>
+      {/* here we are expressing the value of the title prop */}
+      <h3 className="todo__title">{props.title}</h3>
+      {/* and here we are expressing the value of the status */}
+      <p>Status: {props.status}</p>
     </div>
   );
 }
